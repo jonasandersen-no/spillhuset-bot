472b3c8618b9d33aafc5163416b11fc993472a19
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2024-01-01 15:59:22 +0100

    Added minecraft command

---
+++ src/main/java/com/bjoggis/spillhuset/configuration/IdentityAccessTokenResource.java
@@ -0,0 +1,36 @@
+package com.bjoggis.spillhuset.configuration;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class IdentityAccessTokenResource {
+
+  private String accessToken;
+  private String scope;
+  private Long expiresIn;
+
+  public String getAccessToken() {
+    return accessToken;
+  }
+
+  public void setAccessToken(String accessToken) {
+    this.accessToken = accessToken;
+  }
+
+  public String getScope() {
+    return scope;
+  }
+
+  public void setScope(String scope) {
+    this.scope = scope;
+  }
+
+  public Long getExpiresIn() {
+    return expiresIn;
+  }
+
+  public void setExpiresIn(Long expiresIn) {
+    this.expiresIn = expiresIn;
+  }
+}
